b322c0b5abed1bb5b4aafa005b73cd8abdd65869
STARkI2348 <107957195+STARkI2348@users.noreply.github.com>
2025-03-26 21:41:45 +0200

    941/3/26/2025

---
+++ js/task-1.js
@@ -0,0 +1,6 @@
+const items = document.querySelectorAll('li.item');
+console.log(`Number of categories: ${items.length}`);
+items.forEach(item => {
+  console.log(`Category: ${item.querySelector('h2').textContent}`);
+  console.log(`Elements: ${item.querySelectorAll('li').length}`);
+});
