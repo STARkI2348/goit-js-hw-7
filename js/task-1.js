d25a85af605944f9ef16546de64052a82723aa3f
STARkI2348 <107957195+STARkI2348@users.noreply.github.com>
2025-04-02 23:28:56 +0300

    1128/2/4/2025

--- js/task-1.js
+++ js/task-1.js
@@ -4,3 +4,6 @@ items.forEach(item => {
   console.log(`Category: ${item.querySelector('h2').textContent}`);
   console.log(`Elements: ${item.querySelectorAll('li').length}`);
 });
+
+const ulStyle = document.querySelectorAll("ul"); 
+ulStyle.forEach( ul => {ul.style.listStyle = "none";})
